d90efead531004ac9bcf6fe406184b73df96a30b
cornhundred <doctorcorn2002@gmail.com>
2016-07-29 13:01:08 -0400

    added toy category to genes, working on real categories for genes and resources (columns)

--- make_hgram_poster_image.py
+++ make_hgram_poster_image.py
@@ -1,4 +1,6 @@
 import json_scripts
+import numpy as np
+import pandas as pd
 
 def main():
 
@@ -6,43 +8,71 @@ def main():
 
   gene_classes = load_gene_classes()
 
-  make_viz(ini_df, gene_classes)
+  # # make hgram viz with four gene types and no categories
+  # make_viz_no_cats(ini_df, gene_classes)
 
-def make_viz(ini_df, gene_classes):
-  from clustergrammer import Network
+  make_viz_with_cats(ini_df, gene_classes)
 
-  print('make visualization')
+def make_viz_with_cats(ini_df, gene_classes):
 
-  net = Network()
-
-  keep_rows = gene_classes['TF']
-
-  keep_rows.extend(gene_classes['KIN'])
-  keep_rows.extend(gene_classes['GPCR'])
-  keep_rows.extend(gene_classes['IC'])
-
-  print(len(keep_rows))
-  keep_rows = list(set(keep_rows))
-
-  print(len(keep_rows))
+  keep_rows = get_4_fam_genes(gene_classes)
 
   inst_df = ini_df.ix[keep_rows]
 
+  inst_rows = inst_df.index.tolist()
+  inst_cols = inst_df.columns.tolist()
+
+  print(len(inst_rows))
+  print(len(inst_cols))
+
+  new_rows = []
+  # add row categories
+  for inst_row in inst_rows:
+    inst_row =  'Gene: ' + inst_row
+    inst_type = 'Type: type'
+    inst_tuple = (inst_row, inst_type)
+
+    new_rows.append(inst_tuple)
+
+  # make new dataframe
+  mat = inst_df.values
+  col_names = inst_df.columns.tolist()
+
+  new_df = pd.DataFrame(data=mat, columns=col_names, index=new_rows)
+
+  make_viz_json(new_df, 'hgram_4_fam_with_cats.json')
+
+def make_viz_no_cats(ini_df, gene_classes):
+
+  keep_rows = get_4_fam_genes(gene_classes)
+
+  inst_df = ini_df.ix[keep_rows]
+
+  make_viz_json(inst_df, 'hgram_4_fam_no_cats.json')
+
+def get_4_fam_genes(gene_classes):
+  keep_rows = gene_classes['TF']
+  keep_rows.extend(gene_classes['KIN'])
+  keep_rows.extend(gene_classes['GPCR'])
+  keep_rows.extend(gene_classes['IC'])
+  keep_rows = list(set(keep_rows))
+
+  return keep_rows
+
+
+def make_viz_json(inst_df, name):
+  from clustergrammer import Network
+  net = Network()
+
+  filename = 'json/'+name
   load_df = {}
   load_df['mat'] = inst_df
-
   net.df_to_dat(load_df)
-
   net.swap_nan_for_zero()
-
   net.make_clust(views=[])
-
-  net.write_json_to_file('viz', 'json/mult_view.json', 'no-indent')
+  net.write_json_to_file('viz', filename, 'no-indent')
 
 def load_hgram_matrix():
-  import numpy as np
-  import pandas as pd
-
   hgram = json_scripts.load_to_dict('hgram_data_latest/hgram_latest.json')
   hgram['mat'] = np.asarray(hgram['mat'])
 
