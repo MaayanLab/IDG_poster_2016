dcc00ea43eeaaf921332deace066b765db29b15a
cornhundred <doctorcorn2002@gmail.com>
2016-07-29 12:14:41 -0400

    loading data into dataframe, will gather subset of rows and produce toy viz

--- make_hgram_poster_image.py
+++ make_hgram_poster_image.py
@@ -1,16 +1,34 @@
+import json_scripts
+
 def main():
-  import json_scripts
+
+  ini_df = load_hgram_matrix()
+
+  gene_classes = load_gene_classes()
+
+  make_viz(ini_df, gene_classes)
+
+def make_viz(ini_df, gene_classes):
+  print('make visualization')
+
+def load_hgram_matrix():
   import numpy as np
+  import pandas as pd
 
-  # hgram = json_scripts.load_to_dict('hgram_data_latest/hgram_latest.json')
-  # hgram['mat'] = np.asarray(hgram['mat'])
-  # print(hgram.keys())
-  # print(type(hgram['mat']))
-  # print(hgram['mat'].shape)
+  hgram = json_scripts.load_to_dict('hgram_data_latest/hgram_latest.json')
+  hgram['mat'] = np.asarray(hgram['mat'])
 
+  mat = hgram['mat']
+  row_names = hgram['nodes']['row']
+  col_names = hgram['nodes']['col']
+
+  # nodes, and mat
+  ini_df = pd.DataFrame(data=mat, columns=col_names, index=row_names)
+
+  return ini_df
+
+def load_gene_classes():
   gene_classes = json_scripts.load_to_dict('gene_classes_harmonogram.json')
-
-  print(gene_classes.keys())
 
   keep_types = ['TF', 'GPCR', 'IC', 'KIN']
 
@@ -22,7 +40,7 @@ def main():
       inst_genes = gene_classes[inst_class]
       keep_genes.extend(inst_genes)
 
-      print(inst_class)
-  print(len(keep_genes))
+  return gene_classes
+
 
 main()
